b6dc7d956dac8e06d485747f3fd7c34c34ad2db2
Nareshsirvi <nareshsirvi842@gmail.com>
2023-08-27 21:31:25 +0530

    initial commit

---
+++ moviebazaar/src/components/style/homepage.js
@@ -0,0 +1,17 @@
+import Footer from "../common/footer";
+import Header from "../common/header";
+import ImgMediaCard from "../common/main";
+
+function Homepage (){
+
+    return<>
+    <div>
+    <Header/>
+    <h1>Welcome to MOVIEBAZAAR</h1>
+    <ImgMediaCard/>
+    <Footer/>
+    </div>
+    </>
+}
+
+export default Homepage;
